7870365470286ad65f9f920a9584449cb95d9c68
Julian Lopez Padua <julianlopez43013@gmail.com>
2023-12-19 19:23:23 -0300

    navbar con o sin user

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,22 +1,22 @@
+import { Link } from "react-router-dom";
 import useUser from "../hooks/useUser";
+
 export default function Navbar() {
   const { user } = useUser();
   console.log(user);
   return (
     <header>
-      <ul>
-        {user ? (
-          <li>
+       {user ? (
+          <ul>
             <button>Cerrar sesion</button>
             <li>{user.nombre}</li>
-          </li>
+          </ul>
         ) : (
-          <>
-            <li>Login</li>
-            <li>Registro</li>
-          </>
+          <ul>
+            <li><Link to="/login" >Login</Link></li>
+            <li><Link to="/registro" >Registro</Link></li>
+          </ul>
         )}
-      </ul>
     </header>
   );
 }
