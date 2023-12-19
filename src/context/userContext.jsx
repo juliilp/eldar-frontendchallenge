d8ed988890d6e86937ea295378d23d8f78a837b9
Julian Lopez Padua <julianlopez43013@gmail.com>
2023-12-19 19:22:54 -0300

    admin creado al iniciar la app

--- src/context/userContext.jsx
+++ src/context/userContext.jsx
@@ -5,6 +5,20 @@ export const UserContext = createContext();
 export default function UserProvider({ children }) {
   const [user, setUser] = useState(null);
   useEffect(() => {
+    const allUsers = localStorage.getItem("allUsers")
+
+    if(!allUsers) {
+      localStorage.setItem("allUsers", JSON.stringify([
+        {
+          nombre: "admin",
+          descripcion: "Soy descripcion de admin",
+          edad: "soy edad",
+          email: "admin123@gmail.com",
+          password: "123456",
+          isAdmin: true,
+        },
+      ]))
+    }
     const findUser = JSON.parse(localStorage.getItem("userLogin"));
     if (findUser) {
       setUser(findUser);
