b606f7bb82440665e1d1bc07255d8f0c371292fe
Julian Lopez Padua <julianlopez43013@gmail.com>
2023-12-20 18:03:32 -0300

    delete card

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -1,9 +1,24 @@
 import { Link } from "react-router-dom";
-
+import axios from "axios";
 export default function Post({ titulo, body, id }) {
   const usuarioLogueado = JSON.parse(localStorage.getItem("userLogin"));
 
   const isAdmin = usuarioLogueado?.isAdmin;
+
+  const handlerBorrarPost = async () => {
+    
+    if(isAdmin) return alert("No podes borrar siendo admin")
+    
+    try {
+      const result = await axios.delete(`/posts/${id}`) 
+      console.log(result)
+      if(result.status === 200) {
+        alert(`¡Card ${id} borrada!`)
+      }
+    } catch (error) {
+      console.log(error)
+    }
+  }
   return (
     <article
       className={`w-[300px] h-[300px] flex flex-col justify-between py-6 items-center overflow-hidden border border-gray-500 }`}
@@ -17,6 +32,7 @@ export default function Post({ titulo, body, id }) {
           Editar
         </Link>
       )}
+      <button onClick={handlerBorrarPost} >Borrar Post</button>
     </article>
   );
 }
