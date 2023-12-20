9a9fa66efcb26eea19fa84faaae80e6bc04a753b
Julian <julianlopez4301@gmail.com>
2023-12-20 11:57:18 -0300

    fix tailwind

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 export default function Post({ titulo, body }) {
   return (
-    <article className="w-[300px] h-[200px] border bg-red-600">
+    <article className="w-[300px] h-[200px] border">
       <h1>{titulo}</h1>
       <p>{body}</p>
     </article>
