1318a6f07948a87623c736c4a1f6d75db081ffc8
Julian <julianlopez4301@gmail.com>
2023-12-20 11:54:03 -0300

    renderizando post

--- src/vistas/Home.jsx
+++ src/vistas/Home.jsx
@@ -1,9 +1,21 @@
 import Navbar from "../components/Navbar";
+import Post from "../components/Post";
+import usePosts from "../hooks/usePosts";
 export default function Home() {
+  const { data, isLoading, handlerSearch, handlerVerMas, isSuccess, isError } =
+    usePosts();
+  console.log(data);
+  if (isLoading) return <span>Loading..</span>;
+  if (isError) return <span>Error.. algo anda mal</span>;
   return (
     <div>
       <Navbar />
-      Home
+      <section>
+        {isSuccess &&
+          data.map(({ title, body }, key) => {
+            return <Post titulo={title} body={body} key={key} />;
+          })}
+      </section>
     </div>
   );
 }
