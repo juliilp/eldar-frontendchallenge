e59cca37faf6a398ac7cae1906157b7a8e598af6
Julian <julianlopez4301@gmail.com>
2023-12-21 11:22:27 -0300

    lazy loading y suspense

--- src/App.jsx
+++ src/App.jsx
@@ -1,42 +1,72 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import axios from "axios";
-import Login from "./vistas/Login";
-import Registro from "./vistas/Registro";
-import Home from "./vistas/Home";
+import { Suspense, lazy } from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import UserProvider from "./context/userContext";
-import isAuthenticate from "./components/isAuthenticate";
-import CrearPost from "./vistas/CrearPost";
-axios.defaults.baseURL = "https://jsonplaceholder.typicode.com/";
-import { QueryClientProvider, QueryClient } from "@tanstack/react-query";
-import isAdmin from "./components/isAdmin";
-import EditPost from "./vistas/editPost";
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
+import axios from "axios";
+import isAuthenticate from "./components/isAuthenticate";
+import isAdmin from "./components/isAdmin";
+
+axios.defaults.baseURL = "https://jsonplaceholder.typicode.com/";
+
 export default function App() {
-  const queryClient = new QueryClient();
+  const LazyLogin = lazy(() => import("./vistas/Login"));
+  const LazyRegistro = lazy(() => import("./vistas/Registro"));
+  const LazyHome = lazy(() => import("./vistas/Home"));
+  const LazyEditPost = lazy(() => import("./vistas/editPost"));
+  const LazyCrearPost = lazy(() => import("./vistas/CrearPost"));
+
   return (
-    <QueryClientProvider client={queryClient}>
-      <BrowserRouter>
-        <UserProvider>
-          <Navbar />
+    <BrowserRouter>
+      <UserProvider>
+        <Navbar />
+        <Suspense fallback={<h1>Loading...</h1>}>
           <Routes>
-            <Route path="/" Component={Home} />
+            <Route path="/" element={<LazyHome />} />
 
             {/* Rutas protegidas si el usuario está "logueado" */}
             <Route Component={isAuthenticate}>
-              <Route path="/login" Component={Login} />
-              <Route path="/registro" Component={Registro} />
+              <Route
+                path="/login"
+                element={
+                  <Suspense fallback={<h1>Loading...</h1>}>
+                    <LazyLogin />
+                  </Suspense>
+                }
+              />
+              <Route
+                path="/registro"
+                element={
+                  <Suspense fallback={<h1>Loading...</h1>}>
+                    <LazyRegistro />
+                  </Suspense>
+                }
+              />
             </Route>
 
             {/* Ruta protegida que tiene que estar logueado y ser admin */}
             <Route Component={isAdmin}>
-              <Route path="/crearpost" Component={CrearPost} />
-              <Route path="/editpost/:id" Component={EditPost} />
+              <Route
+                path="/crearpost"
+                element={
+                  <Suspense fallback={<h1>Loading...</h1>}>
+                    <LazyCrearPost />
+                  </Suspense>
+                }
+              />
+              <Route
+                path="/editpost/:id"
+                element={
+                  <Suspense fallback={<h1>Loading...</h1>}>
+                    <LazyEditPost />
+                  </Suspense>
+                }
+              />
             </Route>
           </Routes>
-          <Footer />
-        </UserProvider>
-      </BrowserRouter>
-    </QueryClientProvider>
+        </Suspense>
+        <Footer />
+      </UserProvider>
+    </BrowserRouter>
   );
 }
