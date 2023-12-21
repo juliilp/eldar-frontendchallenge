9bb1aeee52cf7b58cb4c43c98bbc1d4f0fe4257e
Julian <julianlopez4301@gmail.com>
2023-12-20 22:53:40 -0300

    Loading

--- src/components/PostLoading.jsx
+++ src/components/PostLoading.jsx
@@ -1,12 +1,16 @@
 export default function PostLoading() {
   return (
-    <article>
-      <div className="bg-neutral-400 animate-pulse w-[200px] h-[20px]" />
-      <div className="flex flex-col gap-1">
-        <div className="bg-neutral-400 animate-pulse w-[200px] h-[5px] " />
-        <div className="bg-neutral-400 animate-pulse w-[200px] h-[5px] " />
-        <div className="bg-neutral-400 animate-pulse w-[200px] h-[5px] " />
-        <div className="bg-neutral-400 animate-pulse w-[100px] h-[5px] " />
+    <article className="max-w-[350px] w-full h-[400px]  flex justify-center items-center flex-col shadow-xl">
+      <div className="bg-neutral-400 animate-pulse w-[200px] h-[15px] rounded-full" />
+      <div className="bg-neutral-400 animate-pulse w-[250px] h-[175px] my-4 rounded-xl" />
+
+      <div className="flex flex-col gap-1 justify-center items-center">
+        <div className="bg-neutral-400 animate-pulse w-[200px] h-[7px] rounded-full " />
+        <div className="bg-neutral-400 animate-pulse w-[200px] h-[7px] rounded-full " />
+        <div className="bg-neutral-400 animate-pulse w-[200px] h-[7px] rounded-full " />
+        <div className="bg-neutral-400 animate-pulse w-[200px] h-[7px] rounded-full " />
+        <div className="bg-neutral-400 animate-pulse w-[200px] h-[7px] rounded-full " />
+        <div className="bg-neutral-400 animate-pulse w-[100px] h-[7px] rounded-full " />
       </div>
     </article>
   );
