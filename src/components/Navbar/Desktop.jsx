4f82f418bbd75633befeff2a0157f424223ad5a6
Julian <julianlopez4301@gmail.com>
2023-12-21 11:22:56 -0300

    animaciones

--- src/components/Navbar/Desktop.jsx
+++ src/components/Navbar/Desktop.jsx
@@ -1,12 +1,16 @@
 import { Link } from "react-router-dom";
 import useUser from "../..//hooks/useUser";
 import { FaUser } from "react-icons/fa";
-
+import { motion } from "framer-motion";
 export default function Desktop({ handlerCerrarSession }) {
   const { user } = useUser();
   return (
     <header className="hidden md:flex fixed top-0 items-center h-16 w-full bg-white px-6 justify-between z-50 ">
-      <article className="flex items-center gap-6">
+      <motion.article
+        className="flex items-center gap-6"
+        initial={{ opacity: 0 }}
+        animate={{ opacity: 1 }}
+      >
         {user && (
           <div className="flex items-center gap-2">
             <FaUser />
@@ -21,50 +25,53 @@ export default function Desktop({ handlerCerrarSession }) {
         >
           Ir a Inicio
         </Link>
-      </article>
+      </motion.article>
 
       {user ? (
         <ul className="flex gap-6 items-center">
           {user.isAdmin && (
-            <>
-              <li>
-                <Link
-                  to="/crearpost"
-                  className="rounded-lg py-[10px] px-10 font-bold bg-[rgb(74,142,204)] text-white"
-                >
-                  Crear post
-                </Link>
-              </li>
-              <li>
-                <button
-                  onClick={handlerCerrarSession}
-                  className="rounded-lg py-2 px-10 font-bold bg-[rgb(74,142,204)] text-white"
-                >
-                  Cerrar sesion
-                </button>
-              </li>
-            </>
+            <li>
+              <Link
+                to="/crearpost"
+                className="rounded-lg py-[10px] px-10 font-bold bg-[rgb(74,142,204)] text-white"
+              >
+                Crear post
+              </Link>
+            </li>
           )}
+          <li>
+            <button
+              onClick={handlerCerrarSession}
+              className="rounded-lg py-2 px-10 font-bold bg-[rgb(74,142,204)] text-white"
+            >
+              Cerrar sesion
+            </button>
+          </li>
         </ul>
       ) : (
-        <ul className="flex gap-6">
-          <li>
+        <motion.ul
+          className="flex gap-6"
+          initial={{ opacity: 0 }}
+          animate={{ opacity: 1 }}
+          transition={{ duration: 0.75, staggerChildren: 0.5 }}
+        >
+          <motion.li>
             <Link
               to="/login"
               className="rounded-lg py-[10px] px-10 font-bold bg-[rgb(74,142,204)] text-white"
             >
               Login
             </Link>
-          </li>
-          <li>
+          </motion.li>
+          <motion.li>
             <Link
               to="/registro"
               className="rounded-lg py-[10px] px-10 font-bold bg-[rgb(74,142,204)] text-white"
             >
               Registro
             </Link>
-          </li>
-        </ul>
+          </motion.li>
+        </motion.ul>
       )}
     </header>
   );
