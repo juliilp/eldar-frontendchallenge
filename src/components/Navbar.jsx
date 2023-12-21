1819d7257b21eac16f2363c420ff6aca713cb972
Julian <julianlopez4301@gmail.com>
2023-12-21 14:48:33 -0300

    ir a "/" al cerrar sesion para evitar bugs

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,11 +2,14 @@ import { useEffect } from "react";
 import useUser from "../hooks/useUser";
 import Desktop from "./Navbar/Desktop";
 import Mobile from "./Navbar/Mobile";
+import { useNavigate } from "react-router-dom";
 
 export default function Navbar() {
   const { setUser } = useUser();
+  const navigate = useNavigate();
   const handlerCerrarSession = () => {
     localStorage.removeItem("userLogin");
+    navigate("/");
     setUser(null);
   };
 
