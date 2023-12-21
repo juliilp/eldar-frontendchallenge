0a4e16f28ba25a233b566de7ece81420a9024862
Julian <julianlopez4301@gmail.com>
2023-12-21 11:23:27 -0300

    delete

--- src/hooks/useLocalStorage.jsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-
-export default function useLocalStorage() {
-  const getUser = localStorage.getItem("userLogin");
-  const allUsers = localStorage.getItem("allUsers");
-  const removeLogin = localStorage.removeItem("userLogin");
-  return [getUser, allUsers, removeLogin];
-}
