dd490c8e7d5abb04c4c457bff1cdfeff4deb8a22
Julian <julianlopez4301@gmail.com>
2023-12-20 22:21:22 -0300

    registro terminado

--- src/vistas/Registro.jsx
+++ src/vistas/Registro.jsx
@@ -1,14 +1,13 @@
 import React, { useEffect, useState } from "react";
-import { string, object, boolean } from 'zod';
+import { string, object, boolean } from "zod";
 import userSchema from "../zod/validacionUser";
-import { useNavigate } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
+import Swal from "sweetalert2";
 const Login = () => {
-  const navigate = useNavigate()
+  const navigate = useNavigate();
   const [errorValidation, setErrorValidation] = useState(null);
   const [dataUser, setDataUser] = useState({
     nombre: "",
-    descripcion: "",
-    edad: "",
     email: "",
     password: "",
     isAdmin: false,
@@ -25,87 +24,90 @@ const Login = () => {
     e.preventDefault();
 
     try {
-      // Validacion con zod 
+      // Validacion con zod
       const allUsers = JSON.parse(localStorage.getItem("allUsers"));
       const findUser = allUsers.find((user) => user.email === dataUser.email);
-      if(findUser) return alert("Ese email ya existe")
       userSchema.parse(dataUser);
+      if (findUser)
+        return Swal.fire({
+          icon: "error",
+          title: "Ya existe alguien con ese email",
+        });
       const usuariosExistentes = JSON.parse(localStorage.getItem("allUsers"));
       const usuariosActualizados = [...usuariosExistentes, dataUser];
       localStorage.setItem("allUsers", JSON.stringify(usuariosActualizados));
-      navigate("/login")
+      navigate("/login");
     } catch (error) {
-      setErrorValidation(error.errors)
+      setErrorValidation(error.errors);
     }
   };
 
   //Use éste formato para no repetir tanto código de html
   const datosUser = [
     {
-      span: "nombre",
+      span: "Nombre",
       name: "nombre",
       type: "text",
       value: dataUser.nombre,
       placeholder: "Escribe tu nombre...",
     },
     {
-      span: "descripcion",
-      name: "descripcion",
-      type: "text",
-      value: dataUser.descripcion,
-      placeholder: "Escribe tu descripcion...",
-    },
-    {
-      span: "edad",
-      name: "edad",
-      type: "text",
-      value: dataUser.edad,
-      placeholder: "Escribe tu edad",
-    },
-    {
-      span: "email:",
+      span: "Email",
       name: "email",
       type: "email",
       value: dataUser.email,
-      placeholder: "Escribe tu email",
+      placeholder: "email@gmail.com",
     },
     {
-      span: "password:",
+      span: "Password",
       name: "password",
       type: "password",
       value: dataUser.password,
-      placeholder: "Escribe tu password...",
+      placeholder: "**************",
     },
   ];
 
   /// Hace que los errores de las validaciones se borren despues de 3 segundos
   useEffect(() => {
-    if(errorValidation?.length > 0) {
+    if (errorValidation?.length > 0) {
       const timer = setTimeout(() => {
-        setErrorValidation(null)
-      }, 3000)
-     return () => clearTimeout(timer)
+        setErrorValidation(null);
+      }, 3000);
+      return () => clearTimeout(timer);
     }
-  },[errorValidation])
+  }, [errorValidation]);
 
   return (
-    <>
+    <main className="w-full h-screen flex flex-col justify-center items-center">
+      <h1 className="text-5xl font-bold text-center ">Crea un usuario</h1>
+      <p className="text-sm text-center dark:text-gray-400 my-10">
+        ¿Ya tenes una cuenta?
+        <Link to="/login" className=" pl-4 focus:underline hover:underline">
+          Ingresa aqui
+        </Link>
+      </p>
       <form>
         {datosUser.map((d, index) => {
           return (
             <article key={index}>
-              <span>{d.span}</span>
+              <span className="block text-sm">{d.span}</span>
               <input
                 type={d.type}
                 name={d.name}
                 onChange={onChangeRegistro}
                 value={d.value}
                 placeholder={d.placeholder}
+                className="rounded w-full border-gray-200 bg-gray-100 p-4 pr-32 text-sm font-medium focus:ring-0 focus:border-gray-200 focus:bg-gray200"
               />
             </article>
           );
         })}
-        <button onClick={handlerSubmit}>Enviar</button>
+        <button
+          onClick={handlerSubmit}
+          className="w-full px-8 py-3 font-semibold bg-black text-white hover:bg-zinc-800 transition-colors rounded"
+        >
+          Registrarme
+        </button>
       </form>
       {errorValidation && (
         <article style={{ color: "red" }}>
@@ -114,7 +116,7 @@ const Login = () => {
           ))}
         </article>
       )}
-    </>
+    </main>
   );
 };
 
