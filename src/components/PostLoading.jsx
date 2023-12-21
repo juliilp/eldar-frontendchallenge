a1522253c2d7058c0f9424d2eb3b24a869cc4e5b
Julian <julianlopez4301@gmail.com>
2023-12-21 16:02:49 -0300

    imagen loading fix

--- src/components/PostLoading.jsx
+++ src/components/PostLoading.jsx
@@ -2,7 +2,7 @@ export default function PostLoading() {
   return (
     <article className="max-w-[350px] w-full h-[400px]  flex justify-center items-center flex-col shadow-xl">
       <div className="bg-neutral-400 animate-pulse w-[200px] h-[15px] rounded-full" />
-      <div className="bg-neutral-400 animate-pulse w-[250px] h-[175px] my-4 rounded-xl" />
+      <div className="bg-neutral-400 animate-pulse w-[250px] h-[175px] my-4 rounded-xl px-4" />
 
       <div className="flex flex-col gap-1 justify-center items-center">
         <div className="bg-neutral-400 animate-pulse w-[200px] h-[7px] rounded-full " />
