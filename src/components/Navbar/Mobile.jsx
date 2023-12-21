628cc83452e18ccd773271e476239ab555ed0a11
Julian <julianlopez4301@gmail.com>
2023-12-21 10:35:01 -0300

    animacion menu

--- src/components/Navbar/Mobile.jsx
+++ src/components/Navbar/Mobile.jsx
@@ -1,8 +1,8 @@
 import { Link } from "react-router-dom";
 import useUser from "../..//hooks/useUser";
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import { FiMenu } from "react-icons/fi";
-
+import { motion, AnimatePresence } from "framer-motion";
 export default function Mobile({ handlerCerrarSession }) {
   const { user } = useUser();
 
@@ -26,53 +26,60 @@ export default function Mobile({ handlerCerrarSession }) {
         <FiMenu onClick={handlerSwitch} size={35} />
       </article>
 
-      {switchMenu && (
-        <article className="absolute top-12 items-center w-full flex justify-center bg-white  py-8 ">
-          {user ? (
-            <ul className="flex flex-col gap-6 items-center">
-              {user.isAdmin && (
-                <>
-                  <li>
-                    <Link
-                      to="/crearpost"
-                      className="rounded-lg py-[10.5px] px-6 font-bold bg-[rgb(74,142,204)] text-white"
-                    >
-                      Crear post
-                    </Link>
-                  </li>
-                  <li>
-                    <button
-                      onClick={handlerCerrarSession}
-                      className="rounded-lg py-2 px-6 font-bold bg-[rgb(74,142,204)] text-white"
-                    >
-                      Cerrar sesion
-                    </button>
-                  </li>
-                </>
-              )}
-            </ul>
-          ) : (
-            <ul className="flex flex-col gap-6">
-              <li>
-                <Link
-                  to="/login"
-                  className="rounded-lg py-[10px] px-[50px] font-bold bg-[rgb(74,142,204)] text-white"
-                >
-                  Login
-                </Link>
-              </li>
-              <li>
-                <Link
-                  to="/registro"
-                  className="rounded-lg py-[10px] px-10 font-bold bg-[rgb(74,142,204)] text-white"
-                >
-                  Registro
-                </Link>
-              </li>
-            </ul>
-          )}
-        </article>
-      )}
+      <AnimatePresence>
+        {switchMenu && (
+          <motion.article
+            initial={{ opacity: 0, top: 0 }}
+            animate={{ opacity: 1, top: 48 }}
+            exit={{ opacity: 0 }}
+            className="absolute top-12 items-center w-full flex justify-center bg-white  py-8 "
+          >
+            {user ? (
+              <ul className="flex flex-col gap-6 items-center">
+                {user.isAdmin && (
+                  <>
+                    <li>
+                      <Link
+                        to="/crearpost"
+                        className="rounded-lg py-[10.5px] px-6 font-bold bg-[rgb(74,142,204)] text-white"
+                      >
+                        Crear post
+                      </Link>
+                    </li>
+                    <li>
+                      <button
+                        onClick={handlerCerrarSession}
+                        className="rounded-lg py-2 px-6 font-bold bg-[rgb(74,142,204)] text-white"
+                      >
+                        Cerrar sesion
+                      </button>
+                    </li>
+                  </>
+                )}
+              </ul>
+            ) : (
+              <ul className="flex flex-col gap-6">
+                <li>
+                  <Link
+                    to="/login"
+                    className="rounded-lg py-[10px] px-[50px] font-bold bg-[rgb(74,142,204)] text-white"
+                  >
+                    Login
+                  </Link>
+                </li>
+                <li>
+                  <Link
+                    to="/registro"
+                    className="rounded-lg py-[10px] px-10 font-bold bg-[rgb(74,142,204)] text-white"
+                  >
+                    Registro
+                  </Link>
+                </li>
+              </ul>
+            )}
+          </motion.article>
+        )}
+      </AnimatePresence>
     </header>
   );
 }
