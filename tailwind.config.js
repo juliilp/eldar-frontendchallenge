8475969016e4685122a4c8363f5dcb9c2d7a6472
Julian <julianlopez4301@gmail.com>
2023-12-20 22:21:02 -0300

    intento de poner box-shadow

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,11 @@ export default {
       fontFamily: {
         Roboto: "'Roboto', sans-serif;",
       },
+      boxShadow: {
+        cardPost: ` 3px 0px 33px 0px rgba(0,0,0,0.75)
+        -webkit-box-shadow: 3px 0px 33px 0px rgba(0,0,0,0.75)
+        -moz-box-shadow: 3px 0px 33px 0px rgba(0,0,0,0.75)`,
+      },
     },
   },
   plugins: [],
